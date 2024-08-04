bf37a1e796eb2f2b2b4763393720cde0febd2a21
adarsh-chaunal <er.adarsh97@gmail.com>
2024-08-04 18:51:04 +0530

    Worked on xUnit and FluentAssertion

--- NetworkUtility.Tests/PingTest/NetworkServiceTests.cs
+++ NetworkUtility.Tests/PingTest/NetworkServiceTests.cs
@@ -4,5 +4,36 @@ using NetworkUtility.Ping;
 namespace NetworkUtility.Tests.PingTest;
 public class NetworkServiceTests
 {
-    
+    [Fact] // helps test runner to know this method needs to be tested.
+    public void NetworkService_SendPing_ReutrnString()
+    {
+        // Arrange - variable, classes, mocks
+        var pingService = new NetworkService();
+
+        // Act
+        var result = pingService.SendPing();
+
+        // Assert (We can use the xUnit's built in Assert method but dev's generally use Fluent Assertion as it is the best framework for showing error messages)
+        result.Should().NotBeNullOrWhiteSpace();
+        result.Should().Be("Success: Ping Sent!");
+        result.Should().Contain("Success", Exactly.Once());
+    }
+
+    [Theory] // just like [Fact], but it lets you pass the variables([InlineData]).
+    [InlineData(1, 1, 2)]
+    [InlineData(2, 2, 4)]
+    public void NetworkService_PingTimeout_ReturnInt(int a, int b, int expected)
+    {
+        // Arrange
+        var pingService = new NetworkService();
+
+        // Act
+        var result = pingService.PingTimeout(a, b);
+
+        // Assert
+        result.Should().Be(expected);
+        result.Should().BeGreaterThanOrEqualTo(2);
+        result.Should().NotBeInRange(-10000, 0);
+
+    }
 }
--- NetworkUtility/Ping/NetworkService.cs
+++ NetworkUtility/Ping/NetworkService.cs
@@ -1,5 +1,15 @@
 ﻿namespace NetworkUtility.Ping;
 public class NetworkService
 {
-    
+    public string SendPing()
+    {
+        //SearchDNS();
+        //BuildPacket();
+        return "Success: Ping Sent!";
+    }
+
+    public int PingTimeout(int a, int b)
+    {
+        return a + b;
+    }
 }
